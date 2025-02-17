63585c1af5a0095e5000e6b4cabcec5bf8526d15
koushal barick <koushal.barick@stockedge.com>
2025-02-17 11:03:29 +0530

    Bottom tab ongoing

---
+++ src/screens/ConfigurationScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {View,Text} from 'react-native'
+
+const CofigurationScreen = () =>{
+    return(
+        <View>
+            <Text>CofigurationScreen</Text>
+        </View>
+    )
+}
+
+export default CofigurationScreen
